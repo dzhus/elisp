d38d0bc3cb71686181b7c49a524c5054aee58008
Dmitry Dzhus <dima@sphinx.net.ru>
2008-08-03 15:05:18 +0400

    Small fix

--- markdown-goodies.el
+++ markdown-goodies.el
@@ -3,7 +3,6 @@
 ;; Copyright (C) 2007, 2008 Dmitry Dzhus
 
 ;; Author: Dmitry Dzhus <mail@sphinx.net.ru>
-;;
 
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
